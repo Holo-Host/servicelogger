7ee40cdb7f4fb39b26aa4400bff3a5d87334cbed
Perry Kundert <perry@hardconsulting.com>
2019-07-17 12:54:44 -0600

    Correct current semver version to 0.0.5-alpha1

--- config.nix
+++ config.nix
@@ -68,8 +68,8 @@ echo "All finished!!!"
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.4-alpha1";
-   previous = "0.0.3-alpha1";
+   current = "0.0.5-alpha1";
+   previous = "v0.0.4";
   };
 
   github = {
