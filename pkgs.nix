fa4f5db287f45c81c529b9a037e5a3e1a8f951b8
Perry Kundert <perry@hardconsulting.com>
2019-08-23 06:26:44 -0600

    Updated to latest holoportos, with n3h pre-packaged

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holopkgs/archive/25a3c006700acd5d364f693d8978ae8ff933caeb.tar.gz";
-  sha256 = "0cnd9rgpf3picr041ns240dkgrydn6rsdavnj4sibf36m6mm2cy8";
+  url = "https://github.com/Holo-Host/holopkgs/archive/27046155c8592bedfdb9d404a97a118c8e06f1aa.tar.gz";
+  sha256 = "0dihimcmm6rzsxc482606fcvb6v64h7hb16b7kr46w98sfvr6qk2";
 })
