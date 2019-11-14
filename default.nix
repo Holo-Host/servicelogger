fbe50d6f237e1db432abbc38fec0353f2140ed0e
Perry Kundert <perry@hardconsulting.com>
2019-11-14 15:48:05 -0700

    Correct Nix build, prepare for build on Mac

--- default.nix
+++ default.nix
@@ -1,8 +1,18 @@
-{ pkgs ? import ./pkgs.nix {} }: with pkgs;
+{ pkgs ? import ./pkgs.nix {} }:
+
+with pkgs;
+
+let
+  inherit (darwin.apple_sdk.frameworks) CoreServices Security;
+in
 
 {
   servicelogger = buildDNA {
     name = "servicelogger";
     src = gitignoreSource ./.;
+
+    nativeBuildInputs = []
+    ++ (callPackage ./dynamodb {}).buildInputs
+    ++ lib.optionals stdenv.isDarwin [ CoreServices ];
   };
 }
