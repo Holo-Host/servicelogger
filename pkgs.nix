42a920b9ebfb8832fc8a1362696dc7c6fa714185
Perry Kundert <perry@hardconsulting.com>
2019-11-23 12:53:53 -0700

    Upgrade holochain-rust (stress testing)

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/3ceab347945e739dfce8c9155bae3a261776014c.tar.gz";
-  sha256 = "17hywrvnabrar3qjp50v8sx7xv9l99nkjhm392a3f276wrwg9a4b";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/06db88d6654cfd01c61763f4d21ef3fb82edd86b.tar.gz";
+  sha256 = "0zla4aip7f93j0q0qfmz3zcpw0cjvis14cy6grd68zghmbszvmk2";
 })
