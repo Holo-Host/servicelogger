88080676521bffabab41cdfb7331bd2785a986c2
Perry Kundert <perry@hardconsulting.com>
2019-11-23 16:29:37 -0700

    Update holochain-rust (sim2h improvements)

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/06db88d6654cfd01c61763f4d21ef3fb82edd86b.tar.gz";
-  sha256 = "0zla4aip7f93j0q0qfmz3zcpw0cjvis14cy6grd68zghmbszvmk2";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/2e7ddc5bab6b4abc5bf53296735ffd95070a05da.tar.gz";
+  sha256 = "0sas672wvc3iak8kgf4z7j46nr684lh782xl7sxdv882nyb8nzsb";
 })
