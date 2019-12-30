389d6f7c1f0bb4e514a9d4ea56d5220992abed89
Perry Kundert <perry@hardconsulting.com>
2019-12-30 13:34:04 -0800

    Update to HC release-0.0.41-alpha4-holo-nixpkgs

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/8b8da7f30d7940abeb463ca5b5a6fd3df1ad7bae.tar.gz";
-  sha256 = "0wckq3fcgwva9mpa1857d7s118x2np0n11v3wiljmxalny1df023";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/96cfe6d2a01ce4c4e87c8d5586cf3cea3b306d8f.tar.gz";
+  sha256 = "065vv08355av492arjlaknw7bzsksr3jrymb4q39pixfdncvlswm";
 })
