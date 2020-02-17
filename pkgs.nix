2c9ed2b0656802d98ff41bd39cacdf941d5edd30
Perry Kundert <perry@hardconsulting.com>
2020-02-17 11:22:39 -0800

    Update holo-nixpkgs get holochain-rust 0.0.42-alpha5

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/aa6826666f94ca70c182f653877fed806fff64c6.tar.gz";
-  sha256 = "1lq7rnsyk2dizysir68p1lf6rsj5w4wkvvm0adj2bh1jnz6ywlhz";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/9f168a89e46c7831e99783d9e2b280ee0c7931b8.tar.gz";
+  sha256 = "0sp06xrwwi6z0qf9snsiqrc7q589hsll9p4z3sarbym2h28w0pd7";
 })
