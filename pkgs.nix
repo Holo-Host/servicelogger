780fa2fd96cd6ae0fac9d3c685fca7e18759a000
Joel <joelulahanna@gmail.com>
2020-09-17 14:21:06 -0500

    update nixpkgs

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/dd8673f1b28028a7cc667fa692680f0f46284ac1.tar.gz";
-  sha256 = "19c4gm40w5aa2d6pk18742ycvfy0djzk0dyxydrk04k3ycq9q1dg";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/cd6197d3d0c484e1eec8d723b511b1ebc6e373e1.tar.gz";
+  sha256 = "0nk2lh89h4v585f50mbjaz50cij0x2r7njppqry1wd588nqjyzmw";
 })
